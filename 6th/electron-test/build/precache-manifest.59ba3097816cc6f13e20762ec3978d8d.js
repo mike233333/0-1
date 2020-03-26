ea9494f0d09ae001c2fc09847b2f209344a6b811
mike233333 <a1732406120@163.com>
2020-03-26 16:59:59 +0800

    update build

---
+++ 6th/electron-test/build/precache-manifest.59ba3097816cc6f13e20762ec3978d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d99068ec90166e564f44492c70e06f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b1905446b57957469e9",
+    "url": "./static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "cad05eea030fc4652305",
+    "url": "./static/css/main.88f8e45f.chunk.css"
+  },
+  {
+    "revision": "4b1905446b57957469e9",
+    "url": "./static/js/2.2a770e75.chunk.js"
+  },
+  {
+    "revision": "cad05eea030fc4652305",
+    "url": "./static/js/main.d18c51e5.chunk.js"
+  },
+  {
+    "revision": "118157d5e120aabf4738",
+    "url": "./static/js/runtime-main.97614990.js"
+  }
+]);
